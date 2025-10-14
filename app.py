ce18a401fff63d5897eebe452674b2cc6d68d252
Priyanshu Garg <39775480+priyanshugarg29@users.noreply.github.com>
2025-10-14 12:10:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,10 @@ columns = data.columns.to_list()
 st.write("[dataset columns extracted to list]")
 st.write(f"THE DATASET HAS {len(columns)} COLUMNS: {columns}")
 
+st.write("[target and feature selection"])
 target = st.selectbox('Select target variable', columns)
 features = st.multiselect('Select feature columns', columns, default=[col for col in columns if col != target])
 
-st.write(f"Selected target variable: {target}")
-st.write(f"Selected {len(features)} feature columns: {features}")
+st.write(f"Selected target variable: {target}, Data_type: {type(target)} (should be <str>)")
+st.write(f"Selected {len(features)} feature columns: {features}, Data_type: {type(features)} (should be <list>)")
 
