a066eac88a744546bfebb5166f751f90aa5aaa8a
Priyanshu Garg <39775480+priyanshugarg29@users.noreply.github.com>
2025-10-14 11:59:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,9 @@ if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
     st.write("First five rows of your data:")
     st.write(data.head())
+
+st.write("DATASET SUMMARY")
+data_columns = data.columns.to_list()
+st.write("[dataset columns extracted to list]")
+st.write(f"THE DATASET HAS {len(data_columns)} COLUMNS: {data_columns}")
+
